7bafcb4ef235a61e2a58812cdcdb66b48c456760
azekiqi <50574703+azekiqi@users.noreply.github.com>
2020-03-03 15:10:41 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Post from './Post'
+
+function App() {
+  return(
+      <div className="app">
+          <Post name="Ariana" message="Kerkoj punetore!!!!!"/>
+          <Post name="Alekta"/>
+          <Post name="Aulona"/>
+          <Post name="Anita"/>
+      </div>
+  );
+}
+
+export default App;
